03a0edb2d34f0ebc2e5f2a948ad7c81afa237eda
Auyon Haque <haqueauyon@gmail.com>
2023-10-23 17:58:03 -0400

    created basic navbar layout

---
+++ src/pages/index.js
@@ -0,0 +1,23 @@
+// pages/index.js
+
+import React from 'react';
+
+const Home = () => {
+return (
+	<div
+	style={{
+		display: 'flex',
+		justifyContent: 'centre',
+		alignItems: 'centre',
+		height: '100vh'
+	}}
+	>
+	<h1>Layout</h1>
+    <p>similar to cashapp layout</p>
+    <p>add snapshots of what user can see</p>
+    <p>locked tabs only available to see after sign up/in </p>
+	</div>
+);
+};
+
+export default Home;
